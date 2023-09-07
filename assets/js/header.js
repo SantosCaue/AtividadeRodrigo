f3b7df2f66faaf6e902e0bc338c61c45f863a637
Cauê <caue.santos40@etec.sp.gov.br>
2023-09-06 21:24:07 -0300

    Padronização de formatos e nomes + Adição Header

---
+++ assets/js/header.js
@@ -0,0 +1,24 @@
+const microversoes = document.getElementsByClassName("microversoes");
+const header = document.getElementsByTagName("header")[0];
+//Acho que pode ser simplificado se usar um DOMEventListener
+header.addEventListener("mouseover", multiCor);
+header.addEventListener("mouseout", resetHeader);
+
+function multiCor() {
+    microversoes[0].style.backgroundColor = "#000";
+    microversoes[1].style.backgroundColor = "#6666FF";
+    microversoes[2].style.backgroundColor = "#D9D9D9";
+    microversoes[3].style.backgroundColor = "#41c1ef";
+    microversoes[4].style.backgroundColor = "#c5d9eb";
+    for (let n = 0; n < microversoes.length; n++) {
+        microversoes[n].querySelectorAll("img")[0].style.display = "inline";
+    }
+}
+function resetHeader() {
+    for (let n = 0; n < microversoes.length; n++) {
+        if (microversoes[n].id != window.location.pathname.replace(/\//g, '').replace(/\.html/g, '')) {
+            microversoes[n].style.backgroundColor = "";
+            microversoes[n].querySelectorAll("img")[0].style.display = "none";
+        }
+    }
+}
