71392a198c3691b7b52e62a660a68249abbd7a37
Cauê <caue.santos40@etec.sp.gov.br>
2023-09-22 18:17:15 -0300

    Header remake + headear bugs fix

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,30 +1,9 @@
-const microversoes = document.getElementsByClassName("microversoes");
-const header = document.getElementsByTagName("header")[0];
-//Acho que pode ser simplificado se usar um DOMEventListener
-header.addEventListener("mouseover", multiCor);
-header.addEventListener("mouseout", resetHeader);
-
-function multiCor() {
-    microversoes[0].style.backgroundColor = "#000";
-    microversoes[1].style.backgroundColor = "#6666FF";
-    microversoes[2].style.backgroundColor = "#D9D9D9";
-    microversoes[3].style.backgroundColor = "#41c1ef";
-    microversoes[4].style.backgroundColor = "#c5d9eb";
-    for (let n = 0; n < microversoes.length; n++) {
-        microversoes[n].querySelectorAll("img")[0].style.display = "inline";
-    }
-    document.getElementById("menuLateral").style.display = "none";
-    document.getElementById("diminuirTamanho").style.display = "none";
-
-}
-function resetHeader() {
-    for (let n = 0; n < microversoes.length; n++) {
-        if (microversoes[n].id != window.location.pathname.split("/")[window.location.pathname.split("/").length-1].replace(/\.html/g, '')) {
-            microversoes[n].style.backgroundColor = "";
-            microversoes[n].querySelectorAll("img")[0].style.display = "none";
+cabecalho = document.getElementsByTagName("header")[0];
+        function header(){
+            cabecalho.style.display = "flex";
+            document.getElementById("fakeheader").style.display = "none";
         }
-    }
-    document.getElementById("menuLateral").style.display = "block";
-    document.getElementById("diminuirTamanho").style.display = "block";
-
-}
+        function desheader(){
+            cabecalho.style.display = "none";
+            document.getElementById("fakeheader").style.display = "flex";
+        }
