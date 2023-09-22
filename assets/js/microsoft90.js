5e9bb76877571093f382c3f2ad9b9b92c82104ce
gabriel-fodinha <127167783+gabriel-fodinha@users.noreply.github.com>
2023-09-21 21:51:23 -0300

    simsimsim

--- assets/js/microsoft90.js
+++ assets/js/microsoft90.js
@@ -2,8 +2,19 @@ function mostrarHorarioAtual() {
     const agora = new Date();
     const hora = agora.getHours();
     const minuto = agora.getMinutes();
-  
-    const horarioAtual = `${hora}:${minuto}`;
+    if (minuto > 10 && hora > 10) {
+      var horarioAtual = `${hora}:${minuto}`;
+
+    } else if (minuto < 10 && hora < 10) {
+       horarioAtual = `0${hora}:0${minuto}`;
+
+    } else if (minuto < 10) {
+      horarioAtual = `${hora}:0${minuto}`;
+
+    } else if (hora < 10) {
+       horarioAtual = `0${hora}:${minuto}`;
+    }
+    
   
     document.getElementById('texto_Clock').innerText =horarioAtual;
   }
