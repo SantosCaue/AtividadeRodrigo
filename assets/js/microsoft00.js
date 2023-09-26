265177949492bad2d503bcb1751c5c7b5400e28a
Cauê <caue.santos40@etec.sp.gov.br>
2023-09-26 14:25:41 -0300

    Carrosel

--- assets/js/microsoft00.js
+++ assets/js/microsoft00.js
@@ -1,3 +1,4 @@
+  iten =0;
   function aparecerJanela(id){
         document.getElementById("Windows").style.display= "none";
         document.getElementById("Bill").style.display= "none";
@@ -15,4 +16,44 @@
         document.getElementById("Xbox").style.display= "none";
         document.getElementById("principal").style.display ="grid";
 
+  }
+
+function voltar(){
+      if(iten == 0){
+            document.getElementsByClassName("versoes")[0].style.display = "none";
+            document.getElementsByClassName("versoes")[3].style.display = "flex";
+            iten = 3;
+            }else if(iten == 1){
+                  document.getElementsByClassName("versoes")[1].style.display = "none";
+                  document.getElementsByClassName("versoes")[0].style.display = "flex";
+                  iten--;
+            }else if(iten == 2){
+                  document.getElementsByClassName("versoes")[2].style.display = "none";
+                  document.getElementsByClassName("versoes")[1].style.display = "flex";
+                  iten--;
+            }else if(iten == 3){
+                  document.getElementsByClassName("versoes")[3].style.display = "none";
+                  document.getElementsByClassName("versoes")[2].style.display = "flex";
+                  iten--;
+            }
+}
+
+  function prox(){
+      if(iten == 0){
+      document.getElementsByClassName("versoes")[0].style.display = "none";
+      document.getElementsByClassName("versoes")[1].style.display = "flex";
+      iten++
+      }else if(iten == 1){
+            document.getElementsByClassName("versoes")[1].style.display = "none";
+            document.getElementsByClassName("versoes")[2].style.display = "flex";
+            iten++
+      }else if(iten == 2){
+            document.getElementsByClassName("versoes")[2].style.display = "none";
+            document.getElementsByClassName("versoes")[3].style.display = "flex";
+            iten++
+      }else if(iten == 3){
+            document.getElementsByClassName("versoes")[3].style.display = "none";
+            document.getElementsByClassName("versoes")[0].style.display = "flex";
+            iten = 0
+      }
   }
