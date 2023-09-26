b4fac68cd3fb4c2da8b1fc77974cfd8a61e6e8b4
Cauê <caue.santos40@etec.sp.gov.br>
2023-09-26 10:09:16 -0300

    Comit Biografia

--- assets/js/paul.js
+++ assets/js/paul.js
@@ -10,22 +10,22 @@ function getCookie(name) {
   }
 if(getCookie("decada") == 'anos70'){
     document.getElementById("cssdec").href = "assets/css/biografia70";
-    document.getElementById("logomicrosoftbio").src = "gates1";
-    document.getElementById("Imagem").src = "gates1";
+    document.getElementById("logomicrosoftbio").src = "assets/imgs/Microsoft_Logo_(1975).png";
+    document.getElementById("Imagem").src = "paul1";
 }else if(getCookie("decada") == 'anos80'){
     document.getElementById("cssdec").href = "assets/css/biografia80";
-    document.getElementById("logomicrosoftbio").src = "gates2";
-    document.getElementById("Imagem").src = "gates2";
+    document.getElementById("logomicrosoftbio").src = "assets/imgs/Microsoft_Logo_(1982).png";
+    document.getElementById("Imagem").src = "paul2";
 }else if(getCookie("decada") == 'anos90'){
     document.getElementById("cssdec").href = "assets/css/biografia90";
-    document.getElementById("logomicrosoftbio").src = "gates3";
-    document.getElementById("Imagem").src = "gates3";
+    document.getElementById("logomicrosoftbio").src = "assets/imgs/Microsoft_Logo_(1987).png";
+    document.getElementById("Imagem").src = "paul3";
 }else if(getCookie("decada") == 'anos00'){
     document.getElementById("cssdec").href = "assets/css/biografia00";
-    document.getElementById("logomicrosoftbio").src = "gates4";
-    document.getElementById("Imagem").src = "gates4";
+    document.getElementById("logomicrosoftbio").src = "assets/imgs/Microsoft_Logo_(1987).png";
+    document.getElementById("Imagem").src = "paul4";
 }else if(getCookie("decada") == 'anos10'){
     document.getElementById("cssdec").href = "assets/css/biografia10";
-    document.getElementById("logomicrosoftbio").src = "gates5";
-    document.getElementById("Imagem").src = "gates5";
+    document.getElementById("logomicrosoftbio").src = "assets/imgs/Microsoft_Logo_(2012).png";
+    document.getElementById("Imagem").src = "paul5";
 }
