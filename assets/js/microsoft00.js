04f445c57c7bb1f2c7cdad494bf16ae844f4e55a
Cauê <caue.santos40@etec.sp.gov.br>
2023-09-25 22:55:52 -0300

    Microsoft 2000 Upload

---
+++ assets/js/microsoft00.js
@@ -0,0 +1,15 @@
+  function aparecerJanela(id){
+        document.getElementById("Windows").style.display= "none";
+        document.getElementById("Bill").style.display= "none";
+        document.getElementById("A").style.display= "none";
+        document.getElementById("Azure").style.display= "none";
+        document.getElementById("Xbox").style.display= "none";
+        document.getElementById(id).style.display= "block";
+  }
+  function fechar(){
+        document.getElementById("Windows").style.display= "none";
+        document.getElementById("Bill").style.display= "none";
+        document.getElementById("A").style.display= "none";
+        document.getElementById("Azure").style.display= "none";
+        document.getElementById("Xbox").style.display= "none";
+  }
